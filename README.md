e7be3efbd310d18ae22b36ae6d02d1f863acf6dd
Christine Nyman <nymanchristine@gmail.com>
2016-07-09 13:29:40 +0200

    Added JavaScript code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hamburger-icon-animate
+_Code by Christine Nyman_
+
+This is a little bit of code to add an animation to the hamburger icon. The bulk of the animation is powered by CSS3 keyframes and raw JavaScript is used to control when each animation is run.
--- script/custom.js
+++ script/custom.js
@@ -1,55 +1,61 @@
 // Script for hamburger animations
-
 const hamburger = document.getElementById('hamburger');
 const bar1 = document.getElementById('bar1');
 const bar2 = document.getElementById('bar2');
 const bar3 = document.getElementById('bar3');
+var stateActive = false;
 
 hamburger.addEventListener('click', animateIcon);
 
 function animateIcon() {
-  var stateActive = false;
 
   if(!stateActive){
+    stateActive = true;
 
-    // Morph to cross icon
     toCross();
+    showMenu();
+  } else{
+    stateActive = false;
 
-  } else {
-
-    // Morph to hamburger icon
     toBurger();
-
+    hideMenu();
   }
 }
 
+// Functions for menu
+function showMenu() {
+  // Insert code to display menu here
+  console.log("Menu visible");
+}
+
+function hideMenu() {
+  // Insert code to hide menu here
+  console.log("Menu hidden");
+}
+
+// Functions to activate animation
+// Morphs hamburger to cross
 function toCross() {
 
-  $('#bar1').addClass('turn-up').removeClass('back-up');
- $('#bar3').addClass('turn-down').removeClass('back-down');
+  // hides the middle line
+  bar2.style.opacity = '0';
 
-  // hides the second bar
-  $('#bar2').animate({
-    opacity: '0'
-  }, 500, function(){
-
-    // slides down mobile navigation
-   $('#mobile-nav').slideDown(500);
-
- });
+  addClass(bar1, 'turn-up');
+  removeClass(bar1, 'back-up');
+  addClass(bar3, 'turn-down');
+  removeClass(bar3, 'back-down');
 
 } // end of toCross()
 
+// Morphs cross to hamburger
 function toBurger() {
 
-  $('#bar3').removeClass('turn-down').addClass('back-down');
-  $('#bar1').removeClass('turn-up').addClass('back-up');
+  removeClass(bar1, 'turn-up')
+  addClass(bar1, 'back-up');
+  removeClass(bar3, 'turn-down')
+  addClass(bar3, 'back-down');
 
-  $('#bar2').animate({
-    opacity: '1'
-  }, 500, function(){
+  // Shows middle line
+  bar2.style.opacity = '1';
 
-   $('#mobile-nav').slideUp(500);
-
- }); // end of closing animate
 } // end of toBurger()
